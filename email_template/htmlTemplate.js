d1e87d9f69e22db6d8bb0cf4fa0e5f92ffb80a1c
AnanthaKannan <sreeananthakannan@gmail.com>
2020-04-15 14:31:20 +0530

    'update'

--- email_template/htmlTemplate.js
+++ email_template/htmlTemplate.js
@@ -10,29 +10,34 @@ module.exports.template = (subject, name, message, otp = '', url = '') => {
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
         <style>
+            *{
+                font-family: Helvetica;
+            }
            .head{
                display: flex;
                align-items: center;
-               margin-bottom: 15px;
+               justify-content: space-between !important;
+               background-color:#dbfbf3;
+               padding: 15px 15px;
+               color:#004b8f;
+               text-align: center;
+               font-size: 23px;
+               font-family: Roboto;
            }
            img{
-               height: 80px;
-               width: 80px;
-               margin-right: 20px;
+               height: 70px;
+               width: 70px;
+               /* margin-right: 20px; */
            }
             .head_a{
                 font-weight: 300;
-                color:#ffffff ;
-                font-size: 20px;
+                margin-top: 0px;
                 margin-bottom: 8px;
             }
             .head_b{
                 font-weight: bold;
-                color:#ffffff ;
-                font-size: 20px;
-                margin-top: 0px;
-                /* margin: 0px; */
-                /*padding: 0px; */
+                /* color:#ffffff ; */
+                margin: 0px;
             }
             .head_c{
                 color:#ffffff ;
@@ -44,53 +49,88 @@ module.exports.template = (subject, name, message, otp = '', url = '') => {
             }
     
             .anc_a {
-                color: #ffffff;
-                text-decoration: none;
-            }
-            .anc_b{
-                display: inline-block;
-                color: #ffffff;
-                text-decoration: none;
-                padding:10px;
-                background-color: #1f305c;
-                text-align: center;
-                border-radius: 5px;
-            }
+                    color: #ffffff;
+                    text-decoration: none;
+                }
+                .anc_b{
+                    display: inline-block;
+                    color: #ffffff;
+                    text-decoration: none;
+                    padding:10px;
+                    background-color: #1f305c;
+                    text-align: center;
+                    border-radius: 5px;
+                }
+                .green{
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    color:#ffffff;
+                    font-size: 21px;
+                    padding: 15px;
+                    background-color: #48d2a0;
+                }
+                .test{
+                    height: 60px;
+                width: 60px;
+                }
+    
+                .dear{
+                    font-size: 18px;
+                    margin-top:0px; 
+                    padding-top: 10px;
+                }
+    
+                .footer{
+                    color: #ffffff;
+                    font-size: 12px;
+                    text-align: center;
+                    padding: 25px 0px;
+                }
         </style>
     </head>
     <body>
-        <div style="background-color: #1f305c; padding: 20px 40px;">
+        <div style="background-color: #1f305c; ">
             <div>
             <div class="head">
-                 <img src="https://res.cloudinary.com/dmzpphfye/image/upload/v1586749370/dam/logo_a_tmpbkr.png" alt="mhaitlog" />
+                <img src="https://res.cloudinary.com/rapidqubechennai/image/upload/v1586935078/mahait/gomLogo1.png" alt="gomLogo" />
                 <div>
                 <p class="head_a">Higher & Technical Education Department </p>
                   <p class="head_b"> Government of Maharashtra</p>
                 </div>
+                <img src="https://res.cloudinary.com/rapidqubechennai/image/upload/v1586935085/mahait/mahait-logo.png" alt="mahaitLogo" />
             </div>
-            <!-- <br /> -->
-            <div style="background-color: #48d2a0;">
-                <p class="head_c">${subject}</p>
-            </div>
-    
-            <div style="background-color: #ffffff; text-align: left; padding: 20px;">
-                
-                <p>Dear ${name},</p>
+            <!-- body -->
+            <div style="margin: 20px 20px;">
+                <div class='green'>
+                    <div>
+                    <p style="margin-top:0px; margin-bottom:6px;">Reminder</p>
+                    <p style="margin:0px;">${subject}</p>
+                    </div>
+                     <img class="test" src="https://res.cloudinary.com/rapidqubechennai/image/upload/v1586935143/mahait/img.svg" alt="test" />
+                </div>
+                <div  style="background-color: #ffffff; text-align: left; padding: 20px;">
+                    <p class="dear" >Dear ${name},</p>
                 <p> ${message} </P>
-                <p style="font-weight: bold;"> ${otp}   </p>
                 ${ anchor }
                 <br/>
+                <p>Thank you.</p>
                 <br/>
                 <hr />
-                    <p>Thank you.</p>
     
-                        <p>Maharashtra H&TE Department</p>
-            </div>
+                <p style="font-size: 14px; color: #1f305c; font-weight: 500;">
+                    Blockchain is the best possible solution for Certificate Attestation & Management for reducing physical efforts and manual processes along with a digital attested certificate, recorded in 
+                    tamper-proof storage with the facility of verifying the authenticity of digital certificate, anywhere anytime. Using Blockchain, we at MahaIT have implemented a global open ecosystem for secure certificate attestation, storage, sharing and verification.
+                </p>
     
-            <div style="color: #ffffff; text-align: center; font-size: 12px;">
-                <br/>
-               <p style="margin-bottom: 5px;"> All Rights Reserved. Developed by MAHAIT  © 2019  |  Privacy Policy, Terms & Conditions  </p>
-                <a class='anc_a' href="http://digitalattestation.mahait.org">www.digitalattestation.mahait.org</a>
+                </div>
+    
+                <!-- footer -->
+                <div class="footer">
+                    <p style="margin-bottom: 5px;"> All Rights Reserved. Developed by MAHAIT  © 2019  |  Privacy Policy, Terms & Conditions  </p>
+                    <a href="https://digitalattestation.mahait.org" style="color: #ffffff;">www.digitalattestation.mahait.org</a>
+                </div>
+    
             </div>
         </div>
         </div>
